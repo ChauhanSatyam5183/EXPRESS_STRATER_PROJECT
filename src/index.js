1620ea496f3e206f0d76800dc6c15fe43f15baa6
ChauhanSatyam5183 <Chauhansatyam5183@gmail.com>
2025-02-05 12:25:05 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express=require('express');
 // const bodyparser=require('body-parser');
+const User=require('./Schema/userSchema.js');
 
 const ServerConfig=require('./config/servercofig');
 const connectdb=require("./config/dbConfig");
+
+
 
 const app=express();
 
@@ -14,5 +17,15 @@ app.listen(ServerConfig.PORT,async ()=>{
      
       await connectdb();
       console.log(`server started at PORT ${ServerConfig.PORT}..`);
-      // console.log(ServerConfig.PORT);
+     
+
+      // const newUser=await User.create({
+      //       email:"a@b.com",
+      //       mobilenumber:"8850795147",
+      //       firstname:"satyam",
+      //       lastname:"chauhan"
+      // });
+
+      // console.log("created new user");
+      // console.log(newUser);
 })
